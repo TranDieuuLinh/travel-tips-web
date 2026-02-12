119791bfc1ae1941eb81114a7bd6e0686ed63ca6
TranDieuuLinh <dieulinh268268@gmail.com>
2026-02-13 00:05:21 +1100

    edit signin

--- packages/frontend/app/navbar.tsx
+++ packages/frontend/app/navbar.tsx
@@ -179,7 +179,7 @@ const Navbar = () => {
             href="/signin"
             className={isActive("/signin") ? "text-neutral-300" : ""}
           >
-            SIGN IN
+            SIGN INnnn
           </Link>
         )}
       </div>
