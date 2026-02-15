ca8e95529170211beb9ea73498201f63f45a03ad
TranDieuuLinh <dieulinh268268@gmail.com>
2026-02-15 23:24:30 +1100

    make who page readable for small screen

--- packages/frontend/app/home/Who.tsx
+++ packages/frontend/app/home/Who.tsx
@@ -8,11 +8,11 @@ type Props ={
 
 const Who = ({whopost}:Props) => {
   return (
-    <div className="bg-[#FBF8F2] px-4 py-5 md:p-10 justify-between items-center flex flex-row">
-      <div className="w-1/2 justify-center flex">
+    <div className="bg-[#FBF8F2] px-4 py-5 md:p-10 justify-between items-center flex flex-col md:flex-row">
+      <div className="md:w-1/2 justify-center flex">
         <Image src="/WhoPic.png" alt="Who Are We Picture" width={200} height={240} priority className="w-26 h-26.5 sm:w-36.5 sm:h-34.5 lg:h-37.5 xl:h-52 xl:w-50"/>
       </div>
-      <div className="w-1/2 justify-center flex flex-col md:pe-4">
+      <div className="md:w-1/2 justify-center flex flex-col md:pe-4">
         <h2 className="font-serif text-[#6D2608] font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl pb-2">
           {" "}
           {whopost.whoTitle}
